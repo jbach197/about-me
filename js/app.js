6dce48a6546e26718aaf0d2b950770318f598503
Jennifer Bach <jmbach1@gmail.com>
2018-03-06 16:47:04 -0800

    added all 5 questions in js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,52 @@
 
 //Prompt with the first question.  Takes user input, converts to upper case and sets variable.
-var homeTown = prompt('I was born in the midwest.  Please enter "yes" or "no"').toUpperCase();
+var homeTown = prompt('I was born in the midwest.  Please enter a "yes" or "no" response.').toUpperCase();
 homeTown = homeTown.toUpperCase();
 
-if(homeTown.toUpperCase() === 'YES') {
-alert('That is correct!');
+//Shows approprate alert based on yes/no response.
+if (homeTown.toUpperCase() === 'YES' || homeTown.toUpperCase() === 'Y') {
+alert('Correct!  I was born in Illinois');
 } else {
 alert('Incorrect, I was born in Illinois');
 }
+
+//Repeast process with second quesiton.
+var favHobby = prompt('My favorite hobby is photography.  Please enter a "yes" or "no" response.').toUpperCase();
+favHobby = favHobby.toUpperCase();
+
+if (favHobby.toUpperCase() === 'YES' || favHobby.toUpperCase() === 'Y') {
+alert('Incorrect.  Travel is my favorite hobby but photography is a close second.');
+} else {
+alert('Correct.  Travel is my favorite hobby but photography is a close second.');
+}
+
+//Repeast process with third quesiton.
+var liveAbroad = prompt('I have lived abroad.  Please enter a "yes" or "no" response.').toUpperCase();
+liveAbroad  = liveAbroad.toUpperCase();
+
+if (liveAbroad.toUpperCase() === 'YES' || liveAbroad.toUpperCase() === 'Y') {
+alert('Correct!  I have lived in Ireland and France');
+} else {
+alert('Incorrect.  I have lived in Ireland and France.');
+}
+
+//Repeast process with fourth quesiton.
+var favCity = prompt('My favorite city is Chicago.  Please enter a "yes" or "no" response.').toUpperCase();
+favCity = favCity.toUpperCase();
+
+if (favCity.toUpperCase() === 'YES' || favCity.toUpperCase() === 'Y') {
+alert('Incorrect.  My favorite city is Paris, I lived in Chicago for most of my life.');
+} else {
+alert('Correct.  Paris is my favorite city, I lived in Chicago for most of my life');
+}
+
+//Repeast process with fifth quesiton.
+var funFact = prompt('I have been paragliding.  Please enter a "yes" or "no" response.').toUpperCase();
+funFact = funFact.toUpperCase();
+
+if (funFact.toUpperCase() === 'YES' || funFact.toUpperCase() === 'Y') {
+alert('Correct!  I have been paragliding twice.');
+} else {
+alert('Incorrect.  I have been paragliding twice.');
+}
+
