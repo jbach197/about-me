7aa044d8afa82fe0105fea5554394d7bfd7b2a1b
Jennifer Bach <jmbach1@gmail.com>
2018-03-08 22:38:13 -0800

    added counter for questions

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ var userName = prompt('Let\'s play a guessing game.  Enter your name to begin');
 alert('Hello ' + userName);
 console.log('User is ' + userName);
 
+var numCorrect = 0
+
 var homeTown = prompt('I was born in Wisconsin.  Please enter a "yes" or "no" response.').toUpperCase();
 homeTown = homeTown.toUpperCase();
 
@@ -11,8 +13,10 @@ if (homeTown.toUpperCase() === 'YES' || homeTown.toUpperCase() === 'Y') {
   alert('Incorrect, I was born in Illinois.');
 } else {
   alert('Correct!  I was born in Illinois');
+  numCorrect ++;
 }
 console.log('User response to born in WI '+ homeTown);
+console.log('Number Correct: ' + numCorrect);
 
 var pets = prompt('I have a cat.  Please enter a "yes" or "no" response.').toUpperCase();
 pets = pets.toUpperCase();
@@ -21,18 +25,22 @@ if (pets.toUpperCase() === 'YES' || pets.toUpperCase() === 'Y') {
   alert('Incorrect.  No pets at the momment.');
 } else {
   alert('Correct! No pet at the momment.');
+  numCorrect ++;
 }
 console.log('User response to cat '+ pets);
+console.log('Number Correct: ' + numCorrect);
 
 var liveAbroad = prompt('I have lived abroad.  Please enter a "yes" or "no" response.').toUpperCase();
 liveAbroad  = liveAbroad.toUpperCase();
 
 if (liveAbroad.toUpperCase() === 'YES' || liveAbroad.toUpperCase() === 'Y') {
   alert('Correct!  I have lived in Dublin, Ireland and Paris, France.');
+  numCorrect ++;
 } else {
   alert('Incorrect.  I have lived in Dublin, Ireland and Paris, France.');
 }
 console.log('User response to live abroad '+ liveAbroad);
+console.log('Number Correct: ' + numCorrect);
 
 var skyDive = prompt('I have been skydiving.  Please enter a "yes" or "no" response.').toUpperCase();
 skyDive = skyDive.toUpperCase();
@@ -41,17 +49,21 @@ if (skyDive.toUpperCase() === 'YES' || skyDive.toUpperCase() === 'Y') {
   alert('Incorrect.  Never been skydiving but paragliding is fun!.');
 } else {
   alert('Correct!  Never been skydiving but paragliding is fun!.');
+  numCorrect ++;
 }
 console.log('User response to skydiving is '+ skyDive);
+console.log('Number Correct: ' + numCorrect);
 
 var champion = prompt('I attended a game where a professional sports team won a championship.  Please enter a "yes" or "no" response.').toUpperCase();
 
 if (champion.toUpperCase() === 'YES' || champion.toUpperCase() === 'Y') {
   alert('Correct!  I attended the 1996 NBA finals game when the Chicago Bulls beat the Seattle SuperSonics.');
+  numCorrect ++;
 } else {
   alert('Incorrect.  I attended the 1996 NBA finals game when the Chicago Bulls beat the Seattle SuperSonics.');
 }
 console.log('User response to champion is '+ champion);
+console.log('Number Correct: ' + numCorrect);
 
 var numToGuess = Math.floor(Math.random() * 30);
 console.log('Number to guess is ' + numToGuess);
@@ -61,6 +73,7 @@ for (var i = 0; i <= 3; i++) {
 if (numUserGuess === numToGuess) {
   alert('Correct guess!!!');
   console.log('Guess is ' + numUserGuess);
+  numCorrect ++;
   break;
 } else if(numUserGuess > numToGuess) {
   console.log('Guess is ' + numUserGuess);
@@ -72,7 +85,7 @@ if (numUserGuess === numToGuess) {
   numUserGuess = parseInt(prompt('Enter a number between 1 and 30.'));
 }
 }
-
+console.log('Number Correct: ' + numCorrect);
 var countryNotVisitArray = ['norway','poland', 'romania', 'monaco', 'scotland', 'hungary', 'croatia', 'ukraine', 'turkey'];
 console.log('Countries not visited ' + countryNotVisitArray);
 var countryUserGuess = prompt('Guess a country in Europe I have NOT visited.').toLowerCase();
@@ -82,6 +95,7 @@ for (var i = 0; i < countryNotVisitArray.length ; i++) {
 if (countryNotVisitArray[i] === countryUserGuess) {
    alert('Correct!  I have not visited the following countries: Norway, Poland, Romania,  Monaco, Scotland, Hungary, Croatia, Ukraine, Turkey');
   console.log('Guess is ' + countryUserGuess);
+  numCorrect ++;
   break;
   } else {
     console.log('Guess is ' + countryUserGuess);
@@ -89,4 +103,6 @@ if (countryNotVisitArray[i] === countryUserGuess) {
     prompt('Guess a country in Europe I have NOT visited.').toLowerCase();
 }
 }
+console.log('Number Correct: ' + numCorrect);
 
+alert('Thanks for playing '+ userName + 'you answered ' + numCorrect + ' of 7 questions correctly.' )
